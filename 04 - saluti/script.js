2e277b816dba40793aff03be5c3f215ca8b7ad27
klausqama <klaussqama@gmail.com>
2024-10-22 19:25:40 +0200

    Add ex 4.

---
+++ 04 - saluti/script.js
@@ -0,0 +1,17 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const name = 'Mario';
+
+
+// Dichiara la funzione qui.
+function sayHello(person) {
+    const hello = `Ciao ${person}`;
+  
+    return hello;
+  }
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(sayHello(name));
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
